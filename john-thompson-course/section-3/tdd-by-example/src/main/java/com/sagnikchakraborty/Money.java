17047ac30529fdf986bde6234b0c15447eb07476
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2026-03-24 01:05:59 +0530

    section-3_tdd-by-example pgm changes

---
+++ john-thompson-course/section-3/tdd-by-example/src/main/java/com/sagnikchakraborty/Money.java
@@ -0,0 +1,63 @@
+package com.sagnikchakraborty;
+
+import java.util.Objects;
+
+public class Money implements Expression {
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public String currency() {
+        return currency;
+    }
+
+    // Factory Methods...
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    @Override
+    public Money reduce(Bank bank, String toCurrency) {
+        return new Money(amount / bank.rate(currency, toCurrency), toCurrency);
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Money)) return false;
+
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
