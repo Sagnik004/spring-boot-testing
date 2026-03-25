a09d0db5f419000557b0dc97aaed8e960c26d331
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2026-03-26 00:16:58 +0530

    Lifecycle methods demo

--- john-thompson-course/section-4/intro-junit5/src/test/java/com.sagnikchakraborty/GreetingTest.java
+++ john-thompson-course/section-4/intro-junit5/src/test/java/com.sagnikchakraborty/GreetingTest.java
@@ -1,13 +1,17 @@
 package com.sagnikchakraborty;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before Class/Before All - I am only called Once at the start!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -25,5 +29,15 @@ class GreetingTest {
     void testHelloWorld() {
         String result = greeting.helloWorld("Sagnik");
         System.out.println(result);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After Class/After All - I am only called Once at the end!!!");
     }
 }
