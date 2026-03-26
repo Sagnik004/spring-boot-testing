d56a17304b4f176e231d72a5901530e1e2924d66
Sagnik Chakraborty <sagnikchakraborty910@yahoo.com>
2026-03-26 13:08:49 +0530

    JUnit Assertions demo

---
+++ john-thompson-course/section-5/testing-java-junit5/src/test/java/com/sagnikchakraborty/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package com.sagnikchakraborty.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong view returned");
+        assertEquals("index", controller.index(), () -> "Another expensive message. " +
+                "Make me only if you have to!");
+    }
+
+    @Test
+    void oupsHandler() {
+        // Performance tip: If we have an expensive operation to generate the error message,
+        // then we can put that in a lambda expression. It gets called and evaluated only
+        // if assertion fails.
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
+                "message to build " +
+                "for my test");
+    }
+}
